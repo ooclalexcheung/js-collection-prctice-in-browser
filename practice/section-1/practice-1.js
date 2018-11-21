246ec403bf9e4d9eb18d8875c8ca79cb3f85bf61
ooclalexcheung <41135345+ooclalexcheung@users.noreply.github.com>
2018-11-21 12:13:49 +0800

    Add files via upload

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -4,18 +4,5 @@
 //   return 'Implement the practice require, and begin changing code in this row';
 // }
 
-let collectSameElements = (listA, listB) => {
-  let map = {}
-  let result = []
-
-  listB.forEach(item => {
-    map[item] = 0
-  })
-  listA.forEach(item => {
-    if (map[item] === 0) {
-      result.push(item)
-    }
-  })
-
-  return result
-}
+function collectSameElements (collectionA, collectionB) {
+  return collectionA.filter(element => collectionB.includes(element))};
--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -4,43 +4,13 @@
 //   return 'Implement the practice require, and begin changing code in this row';
 // }
 
-let collectSameElements = (listA, listB) => {
-  let map = {}
-  let result = []
+function collectSameElements  (collectionA, objectB) {
 
-  listB = flatten(listB)
-  listA = flatten(listA)
+  const ifElementExists = element => objectB.value.includes(element.key);
+  const filteredElement = collectionA.filter(ifElementExists);
+  const mappedElement = filteredElement.map(element => element.key);
 
-  listB.forEach(item => {
-    map[item] = 0
-  })
-  listA.forEach(item => {
-    if (map[item] === 0) {
-      result.push(item)
-    }
-  })
+  return mappedElement;
 
-  return result
-}
+};
 
-let flatten = item => {
-  let result = []
-
-  if (item instanceof Array) {
-    item.forEach(subItem => {
-      let subResult = flatten(subItem)
-      result = result.concat(subResult)
-    })
-  } else if (typeof item === 'object') {
-    for (let i in item) {
-      let subResult = flatten(item[i])
-      result = result.concat(subResult)
-    }
-  }
-  // exit
-  else {
-    return [item]
-  }
-
-  return result
-}
